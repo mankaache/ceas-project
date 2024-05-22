e4577cbe55c44ce75c7ef8f8c1492685cb0bc1fc
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-22 06:35:43 +0100

    update home page carousel

--- src/components/PageSegments/HomeComp/HomeCarousel.tsx
+++ src/components/PageSegments/HomeComp/HomeCarousel.tsx
@@ -1,42 +1,61 @@
 // import React, { useRef, useState } from 'react';
 // Import Swiper React components
-import {commonImages} from '@/assets'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { commonImages } from "@/assets";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-fade";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation, EffectFade } from "swiper/modules";
 
 export default function HomeCarousel() {
-    return (
-        <>
-            <Swiper
-                spaceBetween={30}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={false}
-                modules={[Autoplay, Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide className={'bg-center home-slide  text-white bg-cover bg-no-repeat w-full h-full'} style={{backgroundImage:`url(${commonImages.slide1}`, backgroundRepeat:'no-repeat !important'}}>
-                    <p className={'z-[100]'}>Slide 1</p>
-                    </SwiperSlide>
-                <SwiperSlide  className={'bg-center home-slide  text-white bg-cover bg-no-repeat w-full h-full'} style={{backgroundImage:`url(${commonImages.slide2}`, backgroundRepeat:'no-repeat !important'}}><p className={'z-[100]'}>Slide 1</p></SwiperSlide>
-                <SwiperSlide  className={'bg-center home-slide  text-white bg-cover bg-no-repeat w-full h-full'} style={{backgroundImage:`url(${commonImages.slide3}`, backgroundRepeat:'no-repeat !important'}}><p className={'z-[100]'}>Slide 1</p></SwiperSlide>
-                <SwiperSlide  className={'bg-center home-slide text-white bg-cover bg-no-repeat w-full h-full'} style={{backgroundImage:`url(${commonImages.slide4}`, backgroundRepeat:'no-repeat !important'}}><p className={'z-[100]'}>Slide 1</p></SwiperSlide>
-
-            </Swiper>
-        </>
-    );
+  return (
+    <>
+      <Swiper
+        speed={2000}
+        spaceBetween={10}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={false}
+        modules={[Autoplay, Pagination, Navigation, EffectFade]}
+        effect="fade"
+        fadeEffect={{
+          crossFade: true,
+        }}
+        className="mySwiper"
+        loop={true}
+        a11y={{
+          prevSlideMessage: "Previous slide",
+          nextSlideMessage: "Next slide",
+        }}
+      >
+        {[
+          { image: commonImages.slide1 },
+          { image: commonImages.slide2 },
+          { image: commonImages.slide3 },
+          { image: commonImages.slide4 },
+        ].map((item) => (
+          <SwiperSlide
+            className={
+              "bg-center home-slide text-white bg-cover bg-no-repeat w-full h-full"
+            }
+            style={{
+              backgroundImage: `url(${item.image.src})`,
+              backgroundRepeat: "no-repeat!important",
+            }}
+          />
+        ))}
+      </Swiper>
+    </>
+  );
 }
