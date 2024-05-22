c44db10857eb4b85f5cb56a3099b6959917b7ae1
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-22 18:03:44 +0100

    feat: add sample data

---
+++ src/data/events.ts
@@ -0,0 +1,54 @@
+import { IEvent } from "@/models";
+
+export const EVENTS: IEvent[] = [
+  {
+    id: "1",
+    location: "Bamenda",
+    excerpt:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    title: "International Women's Day Conference",
+    slug: "international-womens-day-conference",
+    image: {
+      //   src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+      alt: "Internship opportunities",
+    },
+    date: "2020-12-12",
+    category: "conferences",
+    content: `# Hello from all the women`,
+  },
+
+  {
+    id: "2",
+    location: "Douala",
+    excerpt:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    title: "International Women's Day Conference",
+    slug: "international-womens-day-conference",
+    image: {
+      //   src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+      alt: "Internship opportunities",
+    },
+    date: "2020-12-12",
+    category: "internships",
+    content: `# Hello`,
+  },
+
+  {
+    id: "3",
+    location: "Yaounde",
+    excerpt:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    title: "International Women's Day Conference",
+    slug: "international-womens-day-conference",
+    image: {
+      //   src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+      alt: "Internship opportunities",
+    },
+    date: "2020-12-12",
+    category: "conferences",
+    content: `# Hello`,
+  },
+];
---
+++ src/data/index.ts
@@ -0,0 +1,11 @@
+export const DocumentCategories: string[] = [];
+export const PhotosCategories: string[] = [];
+export const ArticlesCategories: string[] = [];
+
+export const catergoryMap: { [key: string]: string } = {
+  conferences: "Conferences",
+  "field-trips": "Field trips",
+  internships: "Internships",
+  defenses: "Defenses",
+};
+export * from "./events";
