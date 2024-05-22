8896d1f427cba637c152898bcb6cbe0da2552b12
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-22 06:33:38 +0100

    feat(components): update about page hero section

--- src/components/PageSegments/About/HeroSection.tsx
+++ src/components/PageSegments/About/HeroSection.tsx
@@ -1,11 +1,20 @@
-import {commonImages} from '@/assets'
+import { commonImages } from "@/assets";
 
 const HeroSection = () => {
-    return (
-        <div className={'h-[80vh] home-slide w-full bg-center bg-cover bg-no-repeat flex justify-center items-center'} style={{backgroundImage:`url(${commonImages.Aboutus3})`}}>
-            <p className={'z-[100] text-white'}>HeroSection</p>
-            </div>
-    )
-}
+  return (
+    <div
+      className={
+        "h-[55vh] md:h-[60vh] lg:h-[calc(100vh-150px)] xl:h-[calc(100vh-150px)] 2xl:h-[75vh] w-full bg-center bg-cover bg-no-repeat flex justify-center items-center"
+      }
+      style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${commonImages.Aboutus3.src})`,
+        boxShadow: "inset 0 0 0 1000px rgba(0,0,0,.2)",
+        backgroundBlendMode: "darken",
+      }}
+    >
+      <p className={"z-[100] text-white text-3xl font-bold"}>About Us</p>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
