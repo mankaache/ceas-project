f6dcb2d6db0130e35a25a58472d7722ceaadc5ce
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-22 06:44:02 +0100

    feat(pages): update app root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,32 @@
 import { Footer, Navbar } from "@/components";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "aos/dist/aos.css";
+import "animate.css";
+import React from "react";
+import AOS from "aos";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+  React.useEffect(() => {
+    AOS.init({
+      once: false,
+      offset: 120,
+      startEvent: "DOMContentLoaded",
+      duration: 1000,
+      delay: 400,
+      mirror: true,
+    });
+  }, []);
+
   return (
     <div>
-      <Navbar/>
       <Component {...pageProps} />
-      <Footer/>
-
+      <ToastContainer theme="colored" />
     </div>
-  
-
-);
+  );
 }
+
+export default appWithTranslation(App);
