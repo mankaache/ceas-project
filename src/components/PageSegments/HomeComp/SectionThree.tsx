38b91fd9e3452148f92804077b71b9ef7a2fb875
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-22 06:36:09 +0100

    update homepage section three

--- src/components/PageSegments/HomeComp/SectionThree.tsx
+++ src/components/PageSegments/HomeComp/SectionThree.tsx
@@ -1,27 +1,53 @@
-
-import {FormationCards} from "../Cards";
-import {Button} from "@/components";
+import { FormationCards } from "../Cards";
+import { Button } from "@/components";
 
 const SectionThree = () => {
-    return (
-        <div className={'w-[95%] md:w-[90%] mb-20 mx-auto'}>
-            <h3 className={'text-center text-3xl py-5 font-semibold pb-3'}>Our <span className={' text-secondary'}>Training </span> Programs</h3>
-            <p className={'py-3 text-center mx-auto w-[60%] text-xl mb-10'}>We offer training programs to partner organisations  and individual organisation</p>
-            <div className={'flex flex-wrap gap-6 justify-center md:justify-start items-center'}>
-                <FormationCards title={'Title'} href={'/'}
-                                desc={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'}/>
+  return (
+    <div className={"w-[95%] md:w-[90%] mb-20 mx-auto max-w-screen-2xl"}>
+      <h3 className={"text-center text-3xl py-5 font-semibold pb-1"}>
+        Our <span className={" text-secondary"}>Training </span> Programs
+      </h3>
+      <p
+        className={
+          "py-3 text-center mx-auto w-full md:w-[60%] text-base md:text-lg lg:text-xl mb-6"
+        }
+      >
+        We offer training programs to partner organisations and individual
+        organisation
+      </p>
+      <div className={"flex flex-wrap gap-6 justify-center items-stretch"}>
+        {[
+          {
+            title: "Title",
+            href: "/",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          },
 
-                <FormationCards title={'Title'} href={'/'}
-                                desc={'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'}/>
+          {
+            title: "Title",
+            href: "/",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          },
 
-            </div>
+          {
+            title: "Title",
+            href: "/",
+            desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          },
+        ].map((program) => (
+          <FormationCards
+            title={program.title}
+            href={program.href}
+            desc={program.desc}
+          />
+        ))}
+      </div>
 
+      <div className={"flex mt-12 justify-center items-center"}>
+        <Button href={"/programs"} text={"See All"} />
+      </div>
+    </div>
+  );
+};
 
-            <div className={'flex mt-16 justify-center items-center'}>
-                <Button href={'/programs'} text={'See All'}/>
-            </div>
-        </div>
-    )
-}
-
-export default SectionThree
+export default SectionThree;
