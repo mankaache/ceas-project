12b12944e352bc61dce10df8b406c2e74e01a821
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-22 06:40:34 +0100

    add i18n

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const { i18n } = require("./next-i18next.config");
+
+module.exports = {
+  i18n,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "c2.staticflickr.com",
+        // port: '',
+        // pathname: '/my-bucket/**',
+      },
+    ],
+  },
+};
