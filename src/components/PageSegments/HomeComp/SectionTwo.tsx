5169688dd33f99abd7702fe3de6171bd0e5bf16f
moforemmanuel <moforemmanuel1000@gmail.com>
2024-06-21 07:02:01 +0100

    home components translation

--- src/components/PageSegments/HomeComp/SectionTwo.tsx
+++ src/components/PageSegments/HomeComp/SectionTwo.tsx
@@ -63,25 +63,26 @@ const SectionTwo = () => {
             "font-semibold text-3xl md:text-4xl lg:text-4xl mb-4 border-l-4 border-secondary pl-2"
           }
         >
-          We offer solutions to reduce the{" "}
-          <span className={"text-secondary"}> bad practices </span>in farming
+          Nous proposons des solutions pour réduire{" "}
+          <span className={"text-secondary"}> mauvaises pratiques </span>en
+          agriculture
         </h3>
         <div className="flex flex-col items-center lg:items-start justify-center lg:pl-6 xl:pl-8">
           <p className={"text-lg leading-8 "}>
-            There's a growing need for independent monitoring of how well Congo
-            Basin countries enforce laws around natural resources. While there
-            are many independent groups doing this, they don't work well
-            together due to distrust, data quality concerns, and differing
-            goals. This lack of collaboration has been a problem for over a
-            decade.
+            Il existe un besoin croissant d'un suivi indépendant de la manière
+            dont les pays du bassin du Congo appliquent les lois relatives aux
+            ressources naturelles. Même si de nombreux groupes indépendants font
+            cela, ils ne travaillent pas bien ensemble en raison de la méfiance,
+            de problèmes de qualité des données et d’objectifs divergents. Ce
+            manque de collaboration est un problème depuis plus d’une décennie.
           </p>
           <p className={"text-lg mt-2 leading-8"}>
-            A team of specialists under the organization FLAG has been working
-            since 2014 to improve independent monitoring of forest regulations
-            in the Congo Basin.
+            Une équipe de spécialistes de l’organisation FLAG travaille depuis
+            2014 pour améliorer le contrôle indépendant de la réglementation
+            forestière dans le bassin du Congo.
           </p>
           <div className={"mt-8"}>
-            <Button href={"/about"} text={"See more"} />
+            <Button href={"/about"} text={"Voir plus"} />
           </div>
         </div>
       </div>
