b9e03c00ae1631cc756847c69c7f4a839fd580c7
mankaache <mankaache10@gmail.com>
2025-01-22 13:51:33 +0100

    fixed

--- src/components/PageSegments/HomeComp/HomeCarousel.tsx
+++ src/components/PageSegments/HomeComp/HomeCarousel.tsx
@@ -40,13 +40,14 @@ export default function HomeCarousel() {
         }}
       >
         {[
-        
           { image: commonImages.carousel1 },
-          // { image: commonImages.carousel2 },
+          { image: commonImages.carousel7 },
           { image: commonImages.carousel3 },
           { image: commonImages.carousel4 },
           { image: commonImages.carousel5 },
           { image: commonImages.carousel6 },
+          { image: commonImages.carousel8 },
+          { image: commonImages.carousel9 },
         ].map((item, idx) => (
           <SwiperSlide
             key={idx}
